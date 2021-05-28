1a8f8f47dcabb13b0e5b4e8e7f4a115ab871cbf0
mjzart <serg.dorosh15@gmail.com>
2021-05-28 11:15:21 +0300

    js1 commit

---
+++ Computer_courses/script.js
@@ -0,0 +1,27 @@
+const myData = "15 Jul 2021";
+
+const daysElem = document.getElementById('days');
+const hoursElem = document.getElementById('hours');
+const minutesElem = document.getElementById('minutes');
+const secondsElem = document.getElementById('seconds');
+
+
+function countdown(){
+    const newMyDate = new Date(myData);
+    const correntDate = new Date();
+
+    const totalSeconds = (newMyDate-correntDate)/1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds)% 60;
+
+    daysElem.innerHTML = days;
+    hoursElem.innerHTML = hours;
+    minutesElem.innerHTML = minutes;
+    secondsElem.innerHTML = seconds;
+
+}
+
+setInterval(countdown, 1000);
